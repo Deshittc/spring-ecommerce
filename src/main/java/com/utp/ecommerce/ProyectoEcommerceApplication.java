dbd7b15053ee5975d2fdbd66e4e1c2485b17fe30
david <dtorre007@gmail.com>
2024-11-08 23:33:05 -0500

    crear las clases model, mapeo clases a tablas con JPA, integrado diseño y vistas al proyecto

--- src/main/java/com/utp/ecommerce/ProyectoEcommerceApplication.java
+++ src/main/java/com/utp/ecommerce/ProyectoEcommerceApplication.java
@@ -2,9 +2,9 @@ package com.utp.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+@SpringBootApplication
 public class ProyectoEcommerceApplication {
 
 	public static void main(String[] args) {
