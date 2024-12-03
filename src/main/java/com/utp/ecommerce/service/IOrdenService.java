ce428776fbcd9d5c98d51605a3b02e6b2acb684f
david <dtorre007@gmail.com>
2024-12-03 03:17:48 -0500

    Metodo para generar el numero de la orden

--- src/main/java/com/utp/ecommerce/service/IOrdenService.java
+++ src/main/java/com/utp/ecommerce/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package com.utp.ecommerce.service;
 
+import java.util.List;
+
 import com.utp.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden save (Orden orden);
 }
