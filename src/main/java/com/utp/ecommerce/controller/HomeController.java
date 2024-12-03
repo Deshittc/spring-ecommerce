8e345cdaacf2fdfba58b8fe8801a13c1673ad4cb
david <dtorre007@gmail.com>
2024-12-03 01:32:26 -0500

    Validacion para doble producto y ver el carrito desde la cabecera del template del usuario menu opcion carrito

--- src/main/java/com/utp/ecommerce/controller/HomeController.java
+++ src/main/java/com/utp/ecommerce/controller/HomeController.java
@@ -74,7 +74,13 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio()*cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//Validar que el producto no se añada 2 veces
+		Integer idProducto=producto.getId();
+		boolean ingresado=detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
+		
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
 		
 		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 		
@@ -84,6 +90,41 @@ public class HomeController {
 		
 		return "usuario/carrito";
 	}
+	
+	//Quitar un producto del Carrito
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductoCart(@PathVariable Integer id, Model model) {
+		
+		//Lista nueva de productos
+		List<DetalleOrden> ordenesNuevas=new ArrayList<DetalleOrden>();
+		
+		for(DetalleOrden detalleOrden: detalles) {
+			if(detalleOrden.getProducto().getId()!=id) {
+				ordenesNuevas.add(detalleOrden);
+			}
+		}
+		
+		//Poner la nueva lista con los productos restantes
+		detalles=ordenesNuevas;
+		
+		double sumaTotal=0;
+		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "usuario/carrito";
+	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return"/usuario/carrito";
+	}
+	
 	
 	
 }
